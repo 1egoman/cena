52a14f4b096ba9c912be160549ffcab67ab00546
1egoman <rsg1egoman@gmail.com>
2014-11-09 07:09:03 -0500

    Moved routes to a seperate area

---
+++ app/routes/index.js
@@ -0,0 +1,78 @@
+var fs = require("fs");
+
+// initialize store
+weg = require("../stores/wegmans");
+weg.setStoreId(10052);
+weg.getKnownProducts()
+
+module.exports = function(app) {
+
+
+  // get a product's id
+  app.get('/store/product/id/:pid', function(req, res){
+
+    // get product by id
+    weg.getProductById(req.params.pid, function(product) {
+      res.send( product || "NONE" );
+    })
+
+  });
+
+  // get a product's url
+  app.get('/store/product/url', function(req, res){
+    // get product by name
+    name = querystring.unescape(req.param("url")) || res.send("FAIL")
+    weg.getProductByUrl(name, function(product) {
+      res.send( product || "NONE" );
+    })
+
+  });
+
+
+  // get the list
+  app.get('/list', function(req, res){
+    fs.readFile(__dirname + "/persistant/list.json", function(err, data) {
+      if (!err) {
+        list = JSON.parse(data);
+        res.send({list: list});
+      }
+    });
+  });
+
+  // set the list
+  app.post('/list', function(req, res) {
+    if (req.body.list) {
+      list = req.body.list;
+
+      // set all prices in the known items
+      _.each(list, function(item) {
+        weg.setProductPrice(item.id, item.price);
+      });
+
+      fs.writeFile(__dirname + "/persistant/list.json", JSON.stringify(list, null, 2));
+
+      res.send("OK")
+    }
+  });
+
+
+  // the recipes
+  app.get("/recipe", function(req, res) {
+    fs.readFile(__dirname + "/persistant/recipes.json", function(err, data) {
+      if (!err) {
+        list = JSON.parse(data);
+        res.send({recipes: list});
+      }
+    });
+  });
+
+  app.post('/recipe', function(req, res) {
+    if (req.body.recipe) {
+      recipe = req.body.recipe;
+
+      fs.writeFile(__dirname + "/persistant/recipes.json", JSON.stringify(recipe, null, 2));
+      res.send("OK")
+    }
+  });
+
+}
